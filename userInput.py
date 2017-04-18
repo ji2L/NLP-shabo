0b10593444712c0c3e533b305913fc931c5a2141
Maxime <maxime@ThinkPad.mi.X>
2017-04-18 16:28:20 +0200

    greetings + ending

--- userInput.py
+++ userInput.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import re
+import random
+
+# sentences we'll respond with if the user greeted us
+GOODBYE_RESPONSES = ["Goodbye.", "See you soon !", "Cya.", "Farewell, friend."]
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup",)
+GREETING_RESPONSES = ["Hello.", "Hey !", "Hi."]
 
 def normalise(sent, lang):
     sent = re.sub("\'\'", '"', sent) # two single quotes = double quotes
@@ -30,7 +36,7 @@ def tokenise(sent, lang):
 def tokenise_en(sent):
 
     sent = re.sub("([^ ])\'", r"\1 '", sent) # separate apostrophe from preceding word by a space if no space to left
-    sent = re.sub(" \'", r" ' ", sent) # separate apostrophe from following word if a space if left
+    sent = re.sub(" \'", r" ' ", sent) # separate apostrophe from following word if a space is left
 
     # separate on punctuation
     cannot_precede = ["M", "Prof", "Sgt", "Lt", "Ltd", "co", "etc", "[A-Z]", "[Ii].e", "[eE].g"] # non-exhaustive list
@@ -42,23 +48,38 @@ def tokenise_en(sent):
     return sent
 
 
+def checkGreetings(userInput):
+    for word in userInput:
+        if word in GREETING_KEYWORDS:
+            return True
+    
+    return False
+
+
+def respond(userInput):
+    if checkGreetings(userInput):
+        print(random.choice(GREETING_RESPONSES))
+    else:
+        print("I did not understand, could you reformulate please ?")
+
+
 def dialogue(lang):
     exit = True
 
-    print("Hi, I'm ShaBo.")
+    print("..:://ShaBo\\\\::..")
 
     while(exit):
         rawInput = input(">>>")
 
-        if rawInput == "exit":
+        if rawInput == "bye":
             exit = False
         else:
             normalisedInput = normalise(rawInput, lang)
             tokenisedInput = tokenise(normalisedInput, lang)
-            print(tokenisedInput)
+            respond(tokenisedInput)
 
 
 if __name__ == '__main__':
     dialogue("en")
-    print("See you next time !")
+    print(random.choice(GOODBYE_RESPONSES))
 
